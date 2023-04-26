832f2628fb3a0970a55bde342ce59151c53bc681
Letícia Caldas <leticia.lbc@hotmail.com>
2023-04-26 10:00:04 -0300

    Aplicação

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,13 +1,14 @@
 package aplicacao;
 
-import tabuleirojogo.Posicao;
+import tabuleirojogo.Tabuleiro;
+
 
 public class Programa {
 
 	
 	public static void main(String[] args) {
 		
-		Posicao posicao = new Posicao;
+		Tabuleiro tabuleiro = new Tabuleiro (5,5);
 
 	}
 }
